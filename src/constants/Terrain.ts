9e3bbf21d58f608071fa5bcea3d48b45bfd39207
Kelrogg <61114591+Kelrogg@users.noreply.github.com>
2021-09-03 19:04:33 +0300

    Revert "RRW-8"

--- src/constants/Terrain.ts
+++
@@ -1,5 +0,0 @@
-export enum TerrainId {
-    WATER = 0,
-    MOUNTAIN = 1,
-    RAILWAY = 2
-}  
--- src/constants/TradeCompany.ts
+++
@@ -1,6 +0,0 @@
-export enum TradeCompanyId {
-    WASHINGTON = 1,
-    PRESCOTT = 2,
-    LITTLE_ROCK = 3,
-    BISMARCK = 4
-}  
